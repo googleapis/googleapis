a2e1f3c4b9e928d4520e0358fbdaad964897c0d8
Rebecca Taylor <rebeccataylor@google.com>
2019-02-05 21:57:40 -0800

    Update speech to use YAML anchors

---
+++ code-samples/speech/v1/python/long_running_recognize_long_running_promise_speech_transcribe_async.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# DO NOT EDIT! This is a generated sample ("LongRunningPromise",  "speech_transcribe_async")
+
+# To install the latest published package dependency, execute the following:
+#   pip install google-cloud-speech
+
+import sys
+
+# [START speech_transcribe_async]
+
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+import io
+import six
+
+
+def sample_long_running_recognize(language_code, local_file_path):
+    """Transcribe local audio file asynchronously"""
+
+    # [START speech_transcribe_async_core]
+
+    client = speech_v1.SpeechClient()
+
+    # language_code = 'en-US'
+    # local_file_path = 'Path to local audio file, e.g. /path/audio.wav'
+
+    if isinstance(language_code, six.binary_type):
+        language_code = language_code.decode('utf-8')
+    if isinstance(local_file_path, six.binary_type):
+        local_file_path = local_file_path.decode('utf-8')
+    sample_rate_hertz = 16000
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = {
+        'sample_rate_hertz': sample_rate_hertz,
+        'language_code': language_code,
+        'encoding': encoding
+    }
+    with io.open(local_file_path, 'rb') as f:
+        content = f.read()
+    audio = {'content': content}
+
+    operation = client.long_running_recognize(config, audio)
+
+    print('Waiting for operation to complete...')
+    response = operation.result()
+
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+    # [END speech_transcribe_async_core]
+
+
+# [END speech_transcribe_async]
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--language_code', type=str, default='en-US')
+    parser.add_argument(
+        '--local_file_path',
+        type=str,
+        default='Path to local audio file, e.g. /path/audio.wav')
+    args = parser.parse_args()
+
+    sample_long_running_recognize(args.language_code, args.local_file_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ code-samples/speech/v1/python/recognize_request_speech_transcribe_sync_gcs.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# DO NOT EDIT! This is a generated sample ("Request",  "speech_transcribe_sync_gcs")
+
+# To install the latest published package dependency, execute the following:
+#   pip install google-cloud-speech
+
+import sys
+
+# [START speech_transcribe_sync_gcs]
+
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+import six
+
+
+def sample_recognize(language_code, gcs_uri):
+    """Transcribe audio file from Google Cloud Storage"""
+
+    # [START speech_transcribe_sync_gcs_core]
+
+    client = speech_v1.SpeechClient()
+
+    # language_code = 'en-US'
+    # gcs_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
+
+    if isinstance(language_code, six.binary_type):
+        language_code = language_code.decode('utf-8')
+    if isinstance(gcs_uri, six.binary_type):
+        gcs_uri = gcs_uri.decode('utf-8')
+    sample_rate_hertz = 16000
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = {
+        'sample_rate_hertz': sample_rate_hertz,
+        'language_code': language_code,
+        'encoding': encoding
+    }
+    audio = {'uri': gcs_uri}
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+    # [END speech_transcribe_sync_gcs_core]
+
+
+# [END speech_transcribe_sync_gcs]
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--language_code', type=str, default='en-US')
+    parser.add_argument(
+        '--gcs_uri',
+        type=str,
+        default='gs://cloud-samples-data/speech/brooklyn_bridge.raw')
+    args = parser.parse_args()
+
+    sample_recognize(args.language_code, args.gcs_uri)
+
+
+if __name__ == '__main__':
+    main()
