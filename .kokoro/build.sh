4cefc229a9197236fc0adf02d69b71c0c5cf59de
Google APIs <noreply@google.com>
2020-01-10 13:50:07 -0800

    Synchronize new proto/yaml changes.
    PiperOrigin-RevId: 289158456

---
+++ .kokoro/build.sh
@@ -0,0 +1,15 @@
+#
+# Setup
+#
+mkdir ${KOKORO_ROOT}/tools
+cd ${KOKORO_ROOT}/tools || exit
+curl -L https://github.com/bazelbuild/bazel/releases/download/2.0.0/bazel-2.0.0-linux-x86_64 -o bazel
+chmod +x bazel
+mkdir bazel_root
+
+#
+# Run build and tests
+#
+cd ${KOKORO_ARTIFACTS_DIR}/github/googleapis || exit
+${KOKORO_ROOT}/tools/bazel --keep_going --output_user_root=${KOKORO_ROOT}/tools/bazel_root build //google/...
+${KOKORO_ROOT}/tools/bazel --keep_going --output_user_root=${KOKORO_ROOT}/tools/bazel_root test //google/...
