6db0bbeb6e959dc4a401f7be17ce2fb79e164a5e
Google APIs <noreply@google.com>
2020-08-21 00:59:02 -0700

    Add support for the Bazelisk launcher.
    PiperOrigin-RevId: 327771486

--- .kokoro/setup.sh
+++ .kokoro/setup.sh
@@ -3,17 +3,17 @@
 set -e
 
 #
-# Install bazel
+# Install bazelisk
 #
 mkdir ${KOKORO_ROOT}/tools
 cd ${KOKORO_ROOT}/tools
-curl -L https://github.com/bazelbuild/bazel/releases/download/3.0.0/bazel-3.0.0-linux-x86_64 -o bazel
-chmod +x bazel
+curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.6.1/bazelisk-linux-amd64 -o bazelisk
+chmod +x bazelisk
 mkdir bazel_root
 cd -
 
 # gapic-generator-python requires python 3.6+
 pyenv global 3.6.1
 
-BAZEL=${KOKORO_ROOT}/tools/bazel
+BAZELISK_BIN=${KOKORO_ROOT}/tools/bazelisk
 BAZEL_ROOT=${KOKORO_ROOT}/tools/bazel_root
