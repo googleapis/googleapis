783d7e07d49e0c25199b009324dd90627318756b
Google APIs <noreply@google.com>
2025-03-05 16:21:15 -0800

    build: update Docker image to use JDK 17
    PiperOrigin-RevId: 733907084

--- .kokoro/start.sh
+++ .kokoro/start.sh
@@ -11,4 +11,5 @@ docker container run \
   --workdir ${KOKORO_ROOT} \
   --ipc=host \
   --entrypoint ${KOKORO_ARTIFACTS_DIR}/github/googleapis/.kokoro/build.sh \
-  gcr.io/gapic-images/googleapis:20230531
+  gcr.io/gapic-images/googleapis:prod
+
